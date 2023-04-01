ee00527d84578702d3e9c5f75ae93b2f9fd088b1
Sparsh_Pathak <sparshpathak2002@gmail.com>
2023-04-01 21:14:12 +0530

    started slow

---
+++ Arrays/PrintingSubarrays.cpp
@@ -0,0 +1,22 @@
+/*
+Printing sub arrays of given array
+
+*/
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int size=8;
+    int array[size]={1,2,3,4,5,6,7,8};
+    //brute force approach timecomplexity:O(n*n*n)
+    for(int i=0;i<size;i++){
+        for(int j=i;j<size;j++){
+            for(int k=i;k<=j;k++){
+                cout<<array[k]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
