f8e2578cecfa2142818ea2f5a2541256105d56e7
Sparsh_Pathak <sparshpathak2002@gmail.com>
2023-04-03 23:22:50 +0530

    optimized solution two sum

--- Arrays/TwoSum.cpp
+++ Arrays/TwoSum.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 #include <vector>
 #include<exception>
+#include <unordered_map>
 using namespace std;
 
 vector <int> twoSum(vector <int>& nums,int target){
 
-    vector <int> ans;
-    // brute force and timecomplexity of O(n*n) BAD
+    // vector <int> ans;
+    // // brute force and timecomplexity of O(n*n) BAD
+    // for(int i=0;i<nums.size();i++){
+    //     for(int j=i+1;j<nums.size();j++){
+    //         if(nums[i]+nums[j]==target){
+    //             ans.push_back(i);
+    //             ans.push_back(j);
+    //             break;
+    //         }
+    //     }
+    // }
+
+    //optimized solution using hash map
+    unordered_map <int,int> umap;
     for(int i=0;i<nums.size();i++){
-        for(int j=i+1;j<nums.size();j++){
-            if(nums[i]+nums[j]==target){
-                ans.push_back(i);
-                ans.push_back(j);
-                break;
-            }
+        if(umap.find(target-nums[i])==umap.end()){
+            umap[nums[i]]=i;
+        }
+        else{
+            return {umap[target-nums[i]],i};
         }
     }
-    return ans;
+
+    return {};
 }
 
 int main(){
